d96da3cbdae0c0db2ddf81895f0a72d62f16a737
PatilBhavesh84 <bhaveshpatil8448@gmail.com>
2022-07-05 23:13:24 +0530

    Added:Equality check

---
+++ UC2LineComparison.java
@@ -0,0 +1,39 @@
+package com.bridgeLabz.lineComparison;
+import java.util.Scanner;
+public class UC2LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Line Comparison Problem");
+		int a1,a2,b1,b2;
+		int a3,a4,b3,b4;
+
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter value of x1");
+		a1=sc.nextInt();
+		System.out.println("Enter value of y1");
+		b1=sc.nextInt();
+		System.out.println("Enter value of x2");
+		a2=sc.nextInt();
+		System.out.println("Enter value of y2");
+		b2=sc.nextInt();
+		Double length1=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
+		System.out.println("The length of line1 is" +length1);
+		System.out.println("Enter value of x3");
+		a3=sc.nextInt();
+		System.out.println("Enter value of y3");
+		b3=sc.nextInt();
+		System.out.println("Enter value of x4");
+		a4=sc.nextInt();
+		System.out.println("Enter value of y4");
+		b4=sc.nextInt();
+		Double length2=Math.sqrt((a4-a3)*(a4-a3)+(b4-b3)*(b4-b3));
+		System.out.println("The length of line2 is" +length2);
+
+
+		System.out.println(length1 + ".compareTo " +length2+ ":" +length1.compareTo(length2));
+	}
+
+}
+
+
